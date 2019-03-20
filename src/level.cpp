77bf80e9d3fab918514b80ca763aae9bf97082e3
Josh Winter <winterjoshr@gmail.com>
2019-03-19 22:39:19 -0400

    doors implemented

--- src/level.cpp
+++ src/level.cpp
@@ -1,5 +1,16 @@
 #include "darktimes.h"
 
+struct neighbor_type
+{
+	enum class ntype
+	{
+		LEFT, RIGHT, ABOVE, BELOW
+	};
+
+	int index;
+	ntype type;
+};
+
 struct room
 {
 	room(float xx, float yy, float ww, float hh) : x(xx), y(yy), w(ww), h(hh) {}
@@ -8,76 +19,73 @@ struct room
 	{ return x + w > other.x && x < other.x + other.w && y + h > other.y && y < other.y + other.h; }
 
 	float x, y, w, h;
+	std::vector<neighbor_type> neighbors;
 };
 
-static room build_room(const room*);
+static room build_room(const room*, neighbor_type::ntype*);
 static std::vector<ent::wall> build_walls(const std::vector<room>&);
-static bool no_collisions(const std::vector<room>&, const room&);
+static neighbor_type::ntype opposite(neighbor_type::ntype);
+static void cut_doors(const std::vector<room>&, std::vector<ent::wall>&);
 
 void game::level::build_level(std::vector<ent::wall> &walls, std::vector<ent::furnishing> &furnishings)
 {
 	std::vector<room> rooms;
 
 	// first room
-	rooms.push_back(build_room(NULL));
-	int prev = 0;
+	rooms.push_back(build_room(NULL, NULL));
 
-	const int iterate = mersenne(14, 25);
+	const int target_count = mersenne(30, 35);
+	int tries = 200;
+	int current_room = 0;
 
-	int tries = 100;
-	while(rooms.size() < iterate)
+	while(rooms.size() < target_count)
 	{
-		if(--tries < 0)
+		if(tries-- < 0)
 		{
-			// recurse, try again
+			// start over and try again
+			walls.clear();
+			furnishings.clear();
 			build_level(walls, furnishings);
 			return;
 		}
 
-		const int children = mersenne(0, 5);
+		neighbor_type::ntype type;
+		room candidate = build_room(&rooms.at(current_room), &type);
 
-		for(int j = 0; j < children; ++j)
+		// check for collisions with existing rooms
+		bool collision = false;
+		for(const room &r : rooms)
 		{
-			const room new_room = build_room(&rooms[prev]);
-			if(no_collisions(rooms, new_room))
-				rooms.push_back(new_room);
+			if(r.collide(candidate))
+			{
+				collision = true;
+				break;
+			}
 		}
+		if(collision)
+			continue;
 
-		if(rooms.size() < 5)
-			prev = rooms.size() - 1;
-		else if(!mersenne(5))
-			prev = rooms.size() - mersenne(1, 5);
-		else
-			prev = mersenne(0, 4);
+		// tell the current room about its new neighbor
+		neighbor_type nt;
+		nt.index = rooms.size();
+		nt.type = type;
+		rooms.at(current_room).neighbors.push_back(nt);
+
+		neighbor_type nt2;
+		nt2.index = current_room;
+		nt2.type = opposite(type);
+		candidate.neighbors.push_back(nt2);
+
+		rooms.push_back(candidate);
+
+		if(mersenne(3))
+			++current_room;
 	}
 
-	const std::vector<ent::wall> built = build_walls(rooms);
-	dput("made {} rooms", rooms.size());
-	walls = built;
+	walls = build_walls(rooms);
 }
 
-std::vector<ent::wall> build_walls(const std::vector<room>& rooms)
-{
-	std::vector<ent::wall> walls;
-
-	const float half = ent::wall::THICKNESS / 2.0f;
-
-	for(const room &rm : rooms)
-	{
-		// top
-		walls.push_back(ent::wall(rm.x - half, rm.y + rm.h - half, rm.w + ent::wall::THICKNESS, ent::wall::THICKNESS));
-		// bottom
-		walls.push_back(ent::wall(rm.x - half, rm.y - half, rm.w + ent::wall::THICKNESS, ent::wall::THICKNESS));
-		// right
-		walls.push_back(ent::wall(rm.x + rm.w - half, rm.y - half, ent::wall::THICKNESS, rm.h + ent::wall::THICKNESS));
-		// left
-		walls.push_back(ent::wall(rm.x - half, rm.y - half, ent::wall::THICKNESS, rm.h + ent::wall::THICKNESS));
-	}
-
-	return walls;
-}
-
-room build_room(const room *parent)
+room build_room(const room *parent, neighbor_type::ntype *type)
 {
 	if(parent == NULL)
 	{
@@ -89,31 +97,127 @@ room build_room(const room *parent)
 	const float width = mersenne(4.0f, 10.0f);
 	const float height = mersenne(4.0f, 10.0f);
 
-	const float width_offset = mersenne(-width / 3.0f, width / 3.0f);
-	const float height_offset = mersenne(-height / 3.0f, height / 3.0f);
+	const float width_offset = mersenne(-width / 3.5f, width / 3.5f);
+	const float height_offset = mersenne(-height / 3.5f, height / 3.5f);
 
 	switch(side)
 	{
 		case 0: // above
-			return room(parent->x + width_offset, parent->y + parent->h, width, height);
+			*type = neighbor_type::ntype::ABOVE;
+			return room(parent->x + width_offset, parent->y + parent->h + ent::wall::THICKNESS, width, height);
 		case 1: // below
-			return room(parent->x + width_offset, parent->y - height, width, height);
+			*type = neighbor_type::ntype::BELOW;
+			return room(parent->x + width_offset, parent->y - height - ent::wall::THICKNESS, width, height);
 		case 2: // right
-			return room(parent->x + parent->w, parent->y + height_offset, width, height);
+			*type = neighbor_type::ntype::RIGHT;
+			return room(parent->x + parent->w + ent::wall::THICKNESS, parent->y + height_offset, width, height);
 		case 3: // left
-			return room(parent->x - width, parent->y + height_offset, width, height);
+			*type = neighbor_type::ntype::LEFT;
+			return room(parent->x - width - ent::wall::THICKNESS, parent->y + height_offset, width, height);
 		default:
 			win::bug("no side");
 	}
 }
 
-bool no_collisions(const std::vector<room> &rooms, const room &candidate)
+neighbor_type::ntype opposite(const neighbor_type::ntype type)
+{
+	switch(type)
+	{
+		case neighbor_type::ntype::ABOVE:
+			return neighbor_type::ntype::BELOW;
+		case neighbor_type::ntype::BELOW:
+			return neighbor_type::ntype::ABOVE;
+		case neighbor_type::ntype::LEFT:
+			return neighbor_type::ntype::RIGHT;
+		case neighbor_type::ntype::RIGHT:
+			return neighbor_type::ntype::LEFT;
+		default: win::bug("no correct side");
+	}
+}
+
+static constexpr float DOOR_WIDTH = 1.5f;
+
+std::vector<ent::wall> build_walls(const std::vector<room> &rooms)
+{
+	std::vector<ent::wall> walls;
+
+	for(const room &rm : rooms)
+	{
+		// left
+		walls.push_back(ent::wall(rm.x - (ent::wall::THICKNESS / 2.0f), rm.y - (ent::wall::THICKNESS / 2.0f), ent::wall::THICKNESS, rm.h + ent::wall::THICKNESS));
+
+		// right
+		walls.push_back(ent::wall(rm.x + rm.w - (ent::wall::THICKNESS / 2.0f), rm.y - (ent::wall::THICKNESS / 2.0f), ent::wall::THICKNESS, rm.h + ent::wall::THICKNESS));
+
+		// bottom
+		walls.push_back(ent::wall(rm.x - (ent::wall::THICKNESS / 2.0f), rm.y - (ent::wall::THICKNESS / 2.0f), rm.w + ent::wall::THICKNESS, ent::wall::THICKNESS));
+
+		// top
+		walls.push_back(ent::wall(rm.x - (ent::wall::THICKNESS / 2.0f), rm.y + rm.h - (ent::wall::THICKNESS / 2.0f), rm.w + ent::wall::THICKNESS, ent::wall::THICKNESS));
+	}
+
+	cut_doors(rooms, walls);
+
+	return walls;
+}
+
+void cut_doors(const std::vector<room> &rooms, std::vector<ent::wall> &walls)
 {
 	for(const room &rm : rooms)
 	{
-		if(candidate.collide(rm))
-			return false;
-	}
+		for(const neighbor_type &nt : rm.neighbors)
+		{
+			const bool side = nt.type == neighbor_type::ntype::LEFT || nt.type == neighbor_type::ntype::RIGHT;
+			const room &neighbor = rooms.at(nt.index);
 
-	return true;
+			float start_overlap, end_overlap;
+
+			if(side)
+			{
+				const float start_overlap = std::max(rm.y, neighbor.y);
+				const float end_overlap = std::min(rm.y + rm.h, neighbor.y + neighbor.h);
+
+				const float midpoint = (start_overlap + end_overlap) / 2.0f;
+				const float xpos = nt.type == neighbor_type::ntype::LEFT ? rm.x : (rm.x + rm.w);
+
+				ent::wall door(xpos, midpoint - (DOOR_WIDTH / 2.0f), 0.1f, DOOR_WIDTH);
+
+				for(int i = 0; i < walls.size(); ++i)
+				{
+					if(walls[i].collide(door))
+					{
+						ent::wall bottom(walls[i].x, walls[i].y, ent::wall::THICKNESS, door.y - walls[i].y);
+						ent::wall top(walls[i].x, door.y + door.h, ent::wall::THICKNESS, (walls[i].y + walls[i].h) - (door.y + door.h));
+						walls.push_back(bottom);
+						walls.push_back(top);
+						walls.erase(walls.begin() + i);
+						break;
+					}
+				}
+			}
+			else
+			{
+				const float start_overlap = std::max(rm.x, neighbor.x);
+				const float end_overlap = std::min(rm.x + rm.w, neighbor.x + neighbor.w);
+
+				const float midpoint = (start_overlap + end_overlap) / 2.0f;
+				const float ypos = nt.type == neighbor_type::ntype::BELOW ? rm.y : (rm.y + rm.h);
+
+				ent::wall door(midpoint - (DOOR_WIDTH / 2.0f), ypos, DOOR_WIDTH, 0.1f);
+
+				for(int i = 0; i < walls.size(); ++i)
+				{
+					if(walls[i].collide(door))
+					{
+						ent::wall left(walls[i].x, walls[i].y, door.x - walls[i].x, ent::wall::THICKNESS);
+						ent::wall right(door.x + door.w, walls[i].y, (walls[i].x + walls[i].w) - (door.x + door.w), ent::wall::THICKNESS);
+						walls.push_back(left);
+						walls.push_back(right);
+						walls.erase(walls.begin() + i);
+						break;
+					}
+				}
+			}
+		}
+	}
 }
